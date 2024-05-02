ac11c3e83acdea1ed90171696784631d97c54b8e
Wish <synologynascontrol@gmail.com>
2024-05-02 11:51:08 +0800

    2024/05/02 11:51:07

---
+++ Wish/script.js
@@ -0,0 +1,13 @@
+function fffun(){
+    
+    document.aaa();
+}
+var aaa="docd fashasrfagewhrrewhwehw";
+document.write(aaa.length);
+alert(aaa.length)
+var string =prompt("string");
+for(i=0;i<5;i++){
+    var string =prompt("string");
+    if(string>5) document.write("big")
+    else document.write("small")
+}
